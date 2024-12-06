84db8bc85927c7ec2eda62d76c70fe049c2ff0bf
AliIntas <aliintas052@gmail.com>
2024-12-06 16:25:21 +0300

    Add files via upload

--- DataBaseProject/supplierSignupPage.php
+++ DataBaseProject/supplierSignupPage.php
@@ -14,9 +14,9 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Document</title>
+    <title>Satıcı Kayıt</title>
 </head>
 <body>
-    
+<div class="tenor-gif-embed" data-postid="21779751" data-share-method="host" data-aspect-ratio="1.77778" data-width="80%"><a href="https://tenor.com/view/nah-ceken-dayi-nah-ceken-kesoh-kesoh67-gif-21779751">Nah Ceken Dayi Kesoh GIF</a>from <a href="https://tenor.com/search/nah+ceken+dayi-gifs">Nah Ceken Dayi GIFs</a></div> <script type="text/javascript" async src="https://tenor.com/embed.js"></script>
 </body>
 </html>
