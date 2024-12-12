d8f69ae69b0ec42cef8845d16ee0067474fd05fa
AliIntas <aliintas052@gmail.com>
2024-12-12 10:44:56 +0300

    Add files via upload

--- DataBaseProject/signupPage.php
+++ DataBaseProject/signupPage.php
@@ -1,6 +1,6 @@
 <?php
+session_start();
 if (isset($_POST["submit"])) {
-    session_start();
     include("inc/baglan.php");
 
     $name = ($_POST["name"]);
@@ -9,7 +9,7 @@ if (isset($_POST["submit"])) {
     $address = ($_POST["address"]);
     $phone_number = ($_POST["phone_number"]);
     $email = ($_POST["eposta"]);
-    $password = ($_POST["password"]); // Şifreleme ekliyebiliriz
+    $password = ($_POST["password"]); // Şifreleme eklenebilir
 
     // user tablosuna kayıt ekle
     $sql_user = "INSERT INTO user (email, password) VALUES ('$email', '$password')";
@@ -22,7 +22,7 @@ if (isset($_POST["submit"])) {
                          VALUES ('$user_id', '$name', '$surname', '$birth_date', '$address', '$phone_number')";
 
         if (mysqli_query($baglanti, $sql_customer)) {
-            header('Location:loginPage.php');
+            header('Location: loginPage.php');
             exit();
         } else {
             echo "Müşteri kaydı sırasında bir hata oluştu: " . mysqli_error($baglanti);
@@ -49,36 +49,21 @@ if (isset($_POST["submit"])) {
         <div class="kayit">
             <h2 class="mb-1"><i>Müşteri Kayıt</i> </h2>
             <form method="POST" action="">
-                <!-- Ad -->
                 <input type="text" name="name" placeholder="Ad" required>
-                <!-- Soyad -->
                 <input type="text" name="surname" placeholder="Soyad" required>
-                <!-- Eposta -->
                 <input type="text" name="eposta" placeholder="E-mail" required>
-                <!-- Şifre -->
                 <input type="password" name="password" placeholder="Password" required>
-                <!-- Doğum Tarihi -->
                 <input type="date" name="birth_date" placeholder="Doğum Tarihi" required>
-                <!-- Adres -->
                 <input type="text" name="address" placeholder="Adres" required>
-                <!-- Telefon Numarası -->
                 <input type="tel" name="phone_number" placeholder="Telefon Numarası" maxlength="11" required>
-                <!-- Gönder Butonu -->
-                <input type="submit" class="kayitButton" value="Kayıt Ol">
+                <input type="submit" name="submit" class="kayitButton" value="Kayıt Ol">
             </form>
             <div class="buttonContainer mt-3">
-                <!-- Satıcı Kayıt Ol Butonu 
-                <div class="giris_kayitButton">
-                    <a href="supplierSignupPage.php"> Satıcı Kayıt</a>
-                </div>-->
-
-                <!-- Üye Girişi -->
                 <div class="KullanıcıGiris">
                     <a href="loginPage.php">Üye Girişi</a>
                 </div>
             </div>
         </div>
-
     </div>
 </body>
 
--- DataBaseProject/supplierSignupPage.php
+++ DataBaseProject/supplierSignupPage.php
@@ -10,13 +10,43 @@
 
 
 <!DOCTYPE html>
-<html lang="en">
-<head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Satıcı Kayıt</title>
-</head>
-<body>
+<html lang="tr">
 
+<head>
+    <meta charset="utf-8">
+    <link rel="stylesheet" href="assets/bootstrap.css">
+    <link rel="stylesheet" href="assets/style.css">
+    <title>Kayıt Sayfası</title>
+</head>
+
+<body>
+    <div class="kayitBackground">
+        <div class="kayit">
+            <h2 class="mb-1"><i>Satıcı Kayıt</i> </h2>
+            <form method="POST" action="">
+                <!--Sirket Adı -->
+                <input type="text" name="name" placeholder=" Şirket Adı" required>
+                <!-- Eposta -->
+                <input type="text" name="eposta" placeholder=" Sirket E-mail" required>
+                <!-- Şifre -->
+                <input type="password" name="password" placeholder="Password" required>
+                <!--Sirket Adres -->
+                <input type="text" name="address" placeholder="Sirket Adres" required>
+                <!--Sirket Telefon Numarası -->
+                <input type="tel" name="phone_number" placeholder="Sirket Telefon Numarası" maxlength="11" required>
+                <!-- Gönder Butonu -->
+                <input type="submit" class="kayitButton" value="Kayıt Ol">
+            </form>
+            <div class="buttonContainer mt-3">
+                <!-- Satıcı Kayıt Ol Butonu -->
+                <div class="giris_kayitButton">
+                    <a href="supplierLoginPage.php"> Satıcı Giriş</a>
+                </div>
+            </div>
+        </div>
+    </div>
+
+    </div>
 </body>
+
 </html>
